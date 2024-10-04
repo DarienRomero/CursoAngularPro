47d9952972b216e300f4231ff8c55d7ac5459030
Darien Romero <darien.romero@mediabyte.com.pe>
2024-10-04 18:09:49 -0500

    metatags

--- src/app/pages/contact-page/contact-page.component.ts
+++ src/app/pages/contact-page/contact-page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-contact-page',
@@ -7,6 +8,14 @@ import { Component } from '@angular/core';
   templateUrl: './contact-page.component.html',
   styleUrl: './contact-page.component.css'
 })
-export default class ContactPageComponent {
-  
+export default class ContactPageComponent implements OnInit {
+  private title = inject(Title);
+  private meta = inject(Meta)
+
+  ngOnInit(): void {
+    this.title.setTitle('Contact Page')
+    this.meta.updateTag({name: 'description', content: 'Este es mi Contact Page'})
+    this.meta.updateTag({name: 'og:title', content: 'Este es mi About Page'})
+    this.meta.updateTag({name: 'keywords', content: 'Hola,Mundo,Darien,Romero,Angular,PRO'})
+  }
 }
--- src/app/pages/pricing-page/pricing-page.component.ts
+++ src/app/pages/pricing-page/pricing-page.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { isPlatformServer } from '@angular/common';
+import { Component, inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-pricing-page',
@@ -7,6 +9,19 @@ import { Component } from '@angular/core';
   templateUrl: './pricing-page.component.html',
   styleUrl: './pricing-page.component.css'
 })
-export default class PricingPageComponent {
-  
+export default class PricingPageComponent implements OnInit {
+  private title = inject(Title);
+  private meta = inject(Meta)
+  private platform = inject(PLATFORM_ID)
+
+  ngOnInit(): void {
+    // if(!isPlatformServer(this.platform)){
+    //   document.title = 'Pricing Page'
+    // }
+    this.title.setTitle('Pricing Page')
+    this.meta.updateTag({name: 'description', content: 'Este es mi Pricing Page'})
+    this.meta.updateTag({name: 'og:title', content: 'Este es mi About Page'})
+    this.meta.updateTag({name: 'keywords', content: 'Hola,Mundo,Darien,Romero,Angular,PRO'})
+    console.log('platform', this.platform)
+  }
 }
