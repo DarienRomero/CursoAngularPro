4e5aad896763b09d457617315b9d53a66a010476
Darien Romero <darien.romero@mediabyte.com.pe>
2024-10-09 16:11:05 -0500

    pokemon detail metatags

--- src/app/pages/pokemon-page/pokemon-page.component.ts
+++ src/app/pages/pokemon-page/pokemon-page.component.ts
@@ -2,6 +2,8 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { Pokemon } from '../../pokemons/interfaces/pokemon.interface';
 import { PokemonsService } from '../../pokemons/services/pokemons.service';
 import { ActivatedRoute } from '@angular/router';
+import { tap } from 'rxjs';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'pokemon-page',
@@ -13,12 +15,39 @@ import { ActivatedRoute } from '@angular/router';
 export default class PokemonPageComponent implements OnInit {
   private pokemonsService = inject(PokemonsService)
   private route = inject(ActivatedRoute)
+  private title = inject(Title)
+  private meta = inject(Meta)
+
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get("id");
     if(!id){
       return;
     }
-    this.pokemonsService.loadPokemon(id).subscribe((data)=>this.pokemon.set(data))
+    this.pokemonsService.loadPokemon(id)
+    .pipe(
+      tap(({name, id}) => {
+        const pageTitle = `#${id} - ${name}`
+        const pageDescripcion = `Página del pokemon: ${name}`
+        this.title.setTitle(pageTitle);
+        this.meta.updateTag({
+          name: 'description',
+          content: pageDescripcion
+        })
+        this.meta.updateTag({
+          name: 'og:title',
+          content: `Página del pokemon: ${pageTitle}`
+        })
+        this.meta.updateTag({
+          name: 'og:description',
+          content: pageDescripcion
+        })
+        this.meta.updateTag({
+          name: 'og:image',
+          content: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`
+        })
+      })
+    )
+      .subscribe((data)=>this.pokemon.set(data))
   }
   public pokemon = signal<Pokemon | null>(null);
 }
