e8459fac51d6a857411e6eab5cc77507e8a8d8ba
Darien Romero <darien.romero@mediabyte.com.pe>
2024-10-17 10:50:52 -0500

    pruebas pokemon card

--- src/app/pokemons/components/pokemon-card/pokemon-card.component.spec.ts
+++ src/app/pokemons/components/pokemon-card/pokemon-card.component.spec.ts
@@ -32,4 +32,23 @@ describe('PokemonCardComponent', () => {
     console.log(compiled)
     expect(component).toBeTruthy();
   });
+
+  it('should have the SimplePokemon signal inputValue', () => {
+    const comp = fixture.componentInstance;
+    expect(comp.pokemon()).toEqual(mockPokemon)
+  });
+
+  it('should render the pokemon name and image correctly', () => {
+    const imgElement = compiled.querySelector('img')!
+    expect(imgElement).toBeDefined();
+    expect(imgElement.src).toBe("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png")
+    const nameElement = compiled.querySelector("#pokemon-name")
+    console.log('nameElement', nameElement)
+    expect(nameElement?.textContent?.trim()).toBe("Bulbasaur")
+  });
+
+  it('should have the proper ng-reflect-router-link', () => {
+    const divWithLink = compiled.querySelector('div')
+    expect(divWithLink?.attributes.getNamedItem('ng-reflect-router-link')?.value).toBe(`/pokemons,${mockPokemon.name}`)
+  });
 });
