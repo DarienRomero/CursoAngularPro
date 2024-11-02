2c7bf4846210f29437b0d89ed47494485a9fc651
Darien Romero <darien.romero@mediabyte.com.pe>
2024-11-01 19:25:22 -0500

    complete example

--- src/app/modules/issues/services/issues.service.spec.ts
+++ src/app/modules/issues/services/issues.service.spec.ts
@@ -1,6 +1,7 @@
 import { provideAngularQuery, QueryClient } from "@tanstack/angular-query-experimental"
 import { IssuesService } from "./issues.service"
 import {TestBed} from "@angular/core/testing"
+import { State } from "../actions/interfaces/github-issue.interface"
 
 describe("IssuesService", ()=>{
     let service: IssuesService
@@ -21,4 +22,61 @@ describe("IssuesService", ()=>{
     it('should be created', () => {
         expect(service).toBeTruthy()
     })
+
+    it('should load labels', async () => {
+        const {data} = await service.labelsQuery.refetch();
+
+        expect(data?.length).toBe(30);
+
+        const [label] = data!;
+
+        expect(typeof label.color).toBe('string')
+        expect(typeof label.default).toBe('boolean')
+        expect(typeof label.description).toBe('string')
+        expect(typeof label.id).toBe('number')
+        expect(typeof label.name).toBe('string')
+        expect(typeof label.node_id).toBe('string')
+        expect(typeof label.url).toBe('string')
+    })
+
+    it('should set selected state, OPEN, CLOSED, ALL', async () => {
+        service.showIssuesByState(State.Closed)
+        expect(service.selectedState()).toBe(State.Closed)
+
+        const {data} = await service.issuesQuery.refetch();
+
+        data?.forEach(issue => {
+            expect(issue.state).toBe(State.Closed)
+        })
+
+        service.showIssuesByState(State.Open)
+
+        const {data: dataOpen} = await service.issuesQuery.refetch();
+
+        dataOpen?.forEach(issue => {
+            expect(issue.state).toBe(State.Open)
+        })
+    });
+
+    it('should set selected labels', async () => {
+        const label = 'Accessibility';
+        service.toggleLabel(label);
+        expect(service.selectedLabels().has(label)).toBeTrue()
+
+        const {data} = await service.issuesQuery.refetch();
+
+        data?.forEach(issue => {
+            expect(issue.state).toBe(State.Closed)
+        })
+
+        service.showIssuesByState(State.Open)
+
+        const {data: dataOpen} = await service.issuesQuery.refetch();
+
+        dataOpen?.forEach(issue => {
+            expect(issue.labels.some(l => l.name === label)).toBeTruthy()
+        })
+
+    });
+    
 })
